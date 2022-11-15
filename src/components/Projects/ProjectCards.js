d9572f5e5e770fff7af777974d3ee8635b248b31
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2022-11-15 21:25:21 +0530

    Added custom logo color to bootstrap badge

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -4,20 +4,20 @@ import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 import Badge from 'react-bootstrap/Badge';
-
+import './ProjectCard.css'
 
 function ProjectCards(props) {
   const colorArr = {
-    "Javascript": ["warning", "dark"],
-    "React Js": ["info", "dark"],
-    "API": ["secondary", "light"],
-    "Node Js": ["success", "light"],
-    "Bootstrap": ["primary", "light"],
-    "HTML": ["#800000", "light"],
-    "CSS": ["primary", "light"],
-    "Android": ["success", "light"],
-    "Firebase": ["warning", "dark"],
-    "JAVA": ["secondary", "light"]
+    "Javascript": ["js", "dark"],
+    "React Js": ["react", "dark"],
+    "API": ["api", "light"],
+    "Node Js": ["node", "light"],
+    "Bootstrap": ["bootstrap", "light"],
+    "HTML": ["html", "light"],
+    "CSS": ["css", "light"],
+    "Android": ["android", "dark"],
+    "Firebase": ["firebase", "dark"],
+    "JAVA": ["java", "dark"]
   }
   const getColor = (badge) => {
     return colorArr[badge];
@@ -26,14 +26,14 @@ function ProjectCards(props) {
     <Card className="project-card-view justify-content-center align-items-center">
       <Card.Img style={{ width: "79%" }} variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
-        <Card.Title style={{fontSize:"1.5rem", padding:"0px 10px"}}>{props.title}</Card.Title>
+        <Card.Title style={{ fontSize: "1.5rem", padding: "0px 10px" }}>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "center" }}>
           {props.description}
         </Card.Text>
-
+        {/* bg={getColor(element)[0]} */}
         <div className="m-3">
           {props.badge.map((element) => {
-            return <span key={element}><Badge text={getColor(element)[1]} pill bg={getColor(element)[0]}>{element}</Badge>{' '}</span>
+            return <span key={element}><Badge text={getColor(element)[1]} bg={getColor(element)[0]} >{element}</Badge>{' '}</span>
           })}
         </div>
 
